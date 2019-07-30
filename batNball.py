6d09ae37de1e4c7d92242cd8ca84ae6704c2fa44
A Bokov <lively.bokov@gmail.com>
2019-07-30 18:27:42 -0500

    first commit

---
+++ batNball.py
@@ -0,0 +1,10 @@
+import tkinter
+import time
+from tkinter import messagebox
+
+canvasWidth=750
+canvasHeight=500
+window=tkinter.Tk()
+canvas=tkinter.Canvas(window, width=canvasWidth, height=canvasHeight, bg='dodgerblue4')
+canvas.pack()
+#bat=can
